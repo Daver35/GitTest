a91d9beb222a7f39a7979cab4e544c1c75182d9e
Daver35 <daviddelapaz5@gmail.com>
2017-04-22 00:31:05 +0200

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public PlayerController player;
+
+    private float movingDistance;
+    private Vector3 lastPlayerPos;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+        lastPlayerPos = player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        movingDistance = player.transform.position.x - lastPlayerPos.x;
+        transform.position = new Vector3(transform.position.x + movingDistance, transform.position.y, transform.position.z);
+        lastPlayerPos = player.transform.position;
+	}
+}
